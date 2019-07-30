125c2b217e69f862a0ec01c72961c83c1ebf204c
Apple <apple@Macintosh.local>
2019-07-30 16:39:58 -0400

    Complete ViewFinder

--- ViewFinder/ViewFinder/PhotoTableViewController.swift
+++ ViewFinder/ViewFinder/PhotoTableViewController.swift
@@ -12,8 +12,10 @@ class PhotoTableViewController: UITableViewController {
     
     var photos : [Photos]  = []
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
         func getPhotos() {
             if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
@@ -54,37 +56,44 @@ class PhotoTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "moveToDetail", sender: photos[indexPath.row])
     }
-    override func prepare(for segue:  UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "moveToDetail" {
-            if let photoDetailView =  segue.destination  as? PhotoDetailViewController  {
-                
-                if let photoToSend = sender as? Photos {
-                    photoDetailView.photo = photoToSend
-        }
-                
-        }
-    }
-}
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let photoToDelete = photos[indexPath.row]
+                context.delete(photoToDelete)
+                
+                (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                getPhotos()
+            }
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "moveToDetail" {
+            if let photoDetailView =  segue.destination as? PhotoDetailViewController {
+                
+                if let photoToSend = sender as? Photos {
+                    photoDetailView.photo = photoToSend
+                }
+                
+            }
+        }
     }
-    */
+    }
+    
 
     /*
     // Override to support rearranging the table view.
@@ -112,5 +121,5 @@ class PhotoTableViewController: UITableViewController {
     */
 
     
-}
+
 
